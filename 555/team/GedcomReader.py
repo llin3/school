092e6d61629d5289f5db927c20441f10dd1b431d
Gemma LC Anible <gemma@ramblinations.com>
2011-10-18 23:17:48 -0600

    Test cases pass for testGedcomReader.

--- 555/team/GedcomReader.py
+++ 555/team/GedcomReader.py
@@ -1,57 +1,63 @@
 import re, os.path
 
 class GedcomReader:
-	
-	# Should be read from a separate plain-text file instead of hard-coded.
-	validTags = ('INDI'
-				,'NAME'
-				,'SEX'
-				,'BIRT'
-				,'DEAT'
-				,'FAMC'
-				,'FAMS'
-				,'FAM'
-				,'MARR'
-				,'HUSB'
-				,'WIFE'
-				,'CHIL'
-				,'DIV'
-				,'DATE'
-				,'TRLR'
-				,'NOTE')
+    
+    # Should be read from a separate plain-text file instead of hard-coded.
+    validTags = ('INDI'
+                ,'NAME'
+                ,'SEX'
+                ,'BIRT'
+                ,'DEAT'
+                ,'FAMC'
+                ,'FAMS'
+                ,'FAM'
+                ,'MARR'
+                ,'HUSB'
+                ,'WIFE'
+                ,'CHIL'
+                ,'DIV'
+                ,'DATE'
+                ,'TRLR'
+                ,'NOTE')
 
-	# Given a filename, make sure it exists, it is a GEDCOM file, and it can be
-	# opened.
-	def __init__(self, filename):
-		self.file = None
-		
-		if not os.path.splitext(filename)[1].lower() == '.ged':
-			raise ValueError('File must be a GEDCOM file')
+    # Given a filename, make sure it exists, it is a GEDCOM file, and it can be
+    # opened.
+    def __init__(self, filename):
+        self.file = None
         
-		if not os.path.isfile(filename):
-			raise ValueError('File not found: {}'.format(filename))
-		
-		f = open(filename, encoding = 'utf-8')
-		
-		self.file = f
+        if not os.path.splitext(filename)[1].lower() == '.ged':
+            raise ValueError('File must be a GEDCOM file')
+        
+        if not os.path.isfile(filename):
+            raise ValueError('File not found: {}'.format(filename))
+        
+        f = open(filename, encoding = 'utf-8')
+        
+        self.file = f
 
-	# Close the file handle when the object dies. Not sure if this is the best
-	# way to handle this; may want to use __enter__ and __exit__ instead.
-	def __del__(self):
-		if self.file and not self.file.closed:
-			self.file.close()
+    # Close the file handle when the object dies. Not sure if this is the best
+    # way to handle this; may want to use __enter__ and __exit__ instead.
+    def __del__(self):
+        if self.file and not self.file.closed:
+            self.file.close()
 
-	# Given a GEDCOM line, return the line level
-	def getLevel(self, line):
-		return int(line.split(None, 1)[0])
-	
-	# Given a GEDCOM line, return the tag
-	def getTag(self, line):
-		l = line.rstrip().split(None, 2)
-		
-		tag = l[1]
-		
-		if not self.getLevel(line) and re.search('^@.+@$', tag):
-			tag = l[2]
-		
-		return (tag in self.validTags) and tag or 'Invalid tag'
+    # Read the next line from the GEDCOM file
+    def getLine(self):
+        if self.file.closed or not self.file.readable:
+            raise ValueError('Unable to read from GEDCOM file')
+        
+        return self.file.readline().strip()
+
+    # Parse an entire file into a list of lists
+    def parse(self):
+        if self.file.closed or not self.file.readable:
+            raise ValueError('Unable to read from GEDCOM file')
+        
+        self.file.seek(0)
+        self.parsed = []
+        
+        for line in self.file:
+            l = line.strip().split(None, 2)
+            l[0] = int(l[0])
+            
+            self.parsed.append(l)
--- 555/team/testGedcomReader.py
+++ 555/team/testGedcomReader.py
@@ -4,19 +4,22 @@ import unittest
 class GedFileIO(unittest.TestCase):
 
     def testGedFilenameExt(self):
+        '''The user should be notified when the file supplied is not a GEDCOM file'''
         self.assertRaises(ValueError, ged.GedcomReader, 'not a file')
         self.assertRaises(ValueError, ged.GedcomReader, 'testfiles/testGedFilenameExt.txt')
         
         self.assertIsInstance(ged.GedcomReader('testfiles/testGedFilenameExt.ged'), ged.GedcomReader)
     
     def testReadLine(self):
+        '''Read a line from a GEDCOM file'''
         greader = ged.GedcomReader('testfiles/testReadLine.ged')
         self.assertIsInstance(greader, ged.GedcomReader)
         
-        self.assertEqual(greader.getline(), 'testReadLine line 1')
-        self.assertEqual(greader.getline(), 'testReadLine line 2')
+        self.assertEqual(greader.getLine(), 'testReadLine line 1')
+        self.assertEqual(greader.getLine(), 'testReadLine line 2')
     
     def testParseLine(self):
+        '''Parse a GEDCOM line into data structures'''
         greader = ged.GedcomReader('testfiles/testParseLine.ged')
         self.assertIsInstance(greader, ged.GedcomReader)
         
@@ -26,6 +29,7 @@ class GedFileIO(unittest.TestCase):
         self.assertEqual(len(greader.parsed), 11)
         
         self.assertListEqual(greader.parsed[0], [0, '@I1@', 'INDI'])
+        self.assertListEqual(greader.parsed[6], [1, 'BIRT'])
         self.assertListEqual(greader.parsed[9], [0, '@F1@', 'FAM'])
         self.assertListEqual(greader.parsed[7], [2, 'DATE', '30 MAR 1983'])
         self.assertListEqual(greader.parsed[10], [1, 'WIFE', '@I1@'])
